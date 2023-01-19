49f9b94b341837acffef326ea70c857595555973
github-action-benchmark <github@users.noreply.github.com>
2023-01-19 16:08:30 +0000

    add Pluginop benchmarks (cargo) benchmark result for 5dde82a9904b453a32b6ef9c7bae43c14e159d8f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674138863572,
+  "lastUpdate": 1674144510141,
   "repoUrl": "https://github.com/qdeconinck/pluginop",
   "entries": {
     "Pluginop benchmarks": [
@@ -41,6 +41,54 @@ window.BENCHMARK_DATA = {
             "name": "static memory",
             "value": 7368,
             "range": "± 596",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "quentin.deconinck@uclouvain.be",
+            "name": "Quentin De Coninck",
+            "username": "qdeconinck"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5dde82a9904b453a32b6ef9c7bae43c14e159d8f",
+          "message": "Merge pull request #6 from qdeconinck/inputs-support\n\nadd support to get all inputs in one call + support for print",
+          "timestamp": "2023-01-19T17:02:32+01:00",
+          "tree_id": "71a34127a271cbcd118b3f26269fd1003be8ec20",
+          "url": "https://github.com/qdeconinck/pluginop/commit/5dde82a9904b453a32b6ef9c7bae43c14e159d8f"
+        },
+        "date": 1674144508637,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "run and return",
+            "value": 445,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "memory allocation",
+            "value": 4075846,
+            "range": "± 754959",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "static memory",
+            "value": 3796,
+            "range": "± 90",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "inputs support",
+            "value": 4127,
+            "range": "± 18",
             "unit": "ns/iter"
           }
         ]
